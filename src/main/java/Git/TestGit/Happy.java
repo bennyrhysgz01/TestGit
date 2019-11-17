42a4749310abc35316ba38891f4ee4da526d6335
bennyrhys <bennyrhys@163.com>
2019-11-17 22:15:09 +0800

    hot-fix 第一次提交分支

--- src/main/java/Git/TestGit/Happy.java
+++ src/main/java/Git/TestGit/Happy.java
@@ -4,5 +4,7 @@ public class Happy {
 	public static void main(String[] args){
 		System.out.println("eclipse1");
 		System.out.println("eclipse2");
+		//新的分支给boss
+		System.out.println("hot-fix");
 	}
 }
